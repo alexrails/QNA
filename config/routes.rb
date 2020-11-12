601df5ecbf040b821030602e9d21203e24b15d66
Alex <alexrails@yandex.ru>
2020-11-13 01:12:41 +0300

    Added User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions do
     resources :answers, only: [:new, :create], shallow: true
   end
+
+  root to: "questions#index"
 end
