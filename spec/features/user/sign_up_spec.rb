f4e4a4302d8ce7363ca111169fcb04ed516009f5
Alex <alexrails@yandex.ru>
2020-12-11 00:27:47 +0300

    User can sign up and sign out

---
+++ spec/features/user/sign_up_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  In order to be able ask question
+  I'd like to sign up
+} do
+
+  scenario 'User tries to sign up' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'User tries to sign up with errors' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+  end
+end
